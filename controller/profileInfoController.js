7cdeda51ddb8c75ff55fc166f7c0eee24691f50a
Mustapha Ridwan <mustapharid19@gmail.com>
2025-10-15 20:39:46 +0100

    Project setup with neccesary file and dependencies

---
+++ controller/profileInfoController.js
@@ -0,0 +1,11 @@
+// Profile info & cat fact api
+
+const profile = async (req,res) => {
+  try {
+
+  } catch (error) {
+
+  }
+}
+
+export default profile
---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { cors } from 'cors';
+import env from 'dotenv';
+import profile from './controller/profileInfoController';
+
+// Configurations
+env.config();
+const app = express();
+const port = process.env.PORT
+
+// Middlewares
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(cors())
+
+// Routes
+app.get('/me',profile)
+
+
+// express initialization
+app.listen(port,()=>{
+  console.log(`App running on port -> ${port}`)
+})
